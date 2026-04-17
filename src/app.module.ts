703f28cd4057aedfa61fc07903b12bbd4a39c546
Qynten <qynten@qgibbs.com>
2026-04-17 15:52:50 -0400

    refactor: remove unused APP_GUARD provider from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
+
 import { TournamentModule } from './tournament/tournament.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
